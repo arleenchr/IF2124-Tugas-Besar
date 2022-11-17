18e9703713319e1b2b1ee844f034ed29adfb50e7
Margaretha Olivia <margarethaolivia41@gmail.com>
2022-11-17 21:38:41 +0700

    convert file to cfg

---
+++ convert_to_cfg.py
@@ -0,0 +1,27 @@
+list_terminal = []
+
+
+def file_to_cfg(filename):
+    file = open(filename, 'r')
+    cfg = {}
+
+    row = file.readline()
+    while (row != ''):
+        head, body = row.replace('\n', '').split(' -> ')
+
+        if (head not in cfg.keys()):
+            cfg[head] = [body.split(' ')]
+        else:
+            cfg[head].append(body.split(' '))
+        row = file.readline()
+
+    file.close()
+    return cfg
+
+
+def is_terminal(string):
+    return string in list_terminal
+
+
+def is_variabel(string):
+    return string not in list_terminal
