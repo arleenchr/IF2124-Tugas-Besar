e17ea044aee1d69cc87c5974bd9c71df2613b34a
arleenchr <16521172@mahasiswa.itb.ac.id>
2022-11-25 00:05:46 +0700

    lexer & main
    list token dibuat jadi global variable
    main belum ditest, belum jadi semuanya

---
+++ lexer.py
@@ -0,0 +1,38 @@
+import re #regex
+import sys
+from token import list_token
+
+def lexer(inputText,listToken):
+    # Membuat lexer untuk parsing program js
+    # Contoh:  "int a = 3;" di-split menjadi 5 lexemes: "int, a, =, 3, ;"
+    currentChar = 0 #indeks yang menunjuk tiap karakter dalam input teks
+    currentLine = 1 #indeks yang menunjuk baris dalam input teks
+    lexemes = []
+    
+    while (currentChar<len(inputText)):
+        if (inputText[currentChar]=='\n'):
+            currentLine += 1 # jika baris baru, indeks penunjuk currentLine bertambah
+        
+        for tkn in listToken:
+            # iterasi semua elemen di list_token
+            regexPattern, token = tkn
+            regex = re.compile(regexPattern) # compile regex
+            match = regex.match(inputText,currentChar) # cari token yang match
+            if (match != None):
+                if token:
+                    lexemes.append(token)
+                break
+        if (match == None):
+            print("Syntax Error")
+            sys.exit()
+        else:
+            currentChar = match.end(0) # update currentChar menunjuk ke setelah suatu token
+            
+    return lexemes
+
+def tokenToStr(filename):
+    file = open(filename)
+    text = file.read()
+    file.close()
+    lexemes = lexer(text,list_token)
+    return " ".join(lexemes)
--- parserprogram.py
+++ parserprogram.py
@@ -1,4 +1,8 @@
 from argparse import ArgumentParser
+from convert_to_cfg import file_to_cfg
+from convert_to_cnf import cfg_to_cnf
+from cyk import cyk
+from lexer import tokenToStr
 
 if __name__ == "__main__":
     # command parser
@@ -7,9 +11,8 @@ if __name__ == "__main__":
     args = argParser.parse_args()
     
     # cek
-    '''
-    if ():
+    if (cyk(cfg_to_cnf(file_to_cfg("grammar.txt")),tokenToStr(args.filename))):
         print("Accepted")
     else:
         print("Syntax Error")
-    '''
+    
