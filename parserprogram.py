aeff1df016ee84de7c78fca376fd3a15ad949e50
AustinPardosi <88770149+AustinPardosi@users.noreply.github.com>
2022-11-25 17:09:36 +0700

    Add color

--- parserprogram.py
+++ parserprogram.py
@@ -3,6 +3,16 @@ from convert_to_cfg import file_to_cfg
 from convert_to_cnf import cfg_to_cnf
 from cyk import cyk
 from lexer import tokenToStr
+from colorama import Fore
+
+def red() :
+    print("\033[31m", end='')
+
+def green() :
+    print("\033[32m", end='')
+
+def reset() :
+    print("\033[0m", end='')
 
 if __name__ == "__main__":
     # command parser
@@ -14,7 +24,11 @@ if __name__ == "__main__":
     print(tokenToStr(args.filename))
     # cek
     if (cyk(cfg_to_cnf(file_to_cfg("grammar.txt")),tokenToStr(args.filename))):
+        green()
         print("Accepted")
+        reset()
     else:
+        red()
         print("Syntax Error")
+        reset()
     
